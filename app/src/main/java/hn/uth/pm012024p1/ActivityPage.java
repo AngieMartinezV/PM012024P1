bd0feb904e7772264c64683a4267036c9b27c269
Angie-Martinezz <123768282+Angie-Martinezz@users.noreply.github.com>
2024-01-25 20:59:20 -0600

    Cambios realizados el 25/01/2024

--- app/src/main/java/hn/uth/pm012024p1/ActivityPage.java
+++ app/src/main/java/hn/uth/pm012024p1/ActivityPage.java
@@ -2,13 +2,27 @@ package hn.uth.pm012024p1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class ActivityPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page);
+
+        Intent intent = getIntent();
+        String nomb = intent.getStringExtra("Nombre:");
+        String apell = intent.getStringExtra("Apellido:");
+        String telef = intent.getStringExtra("Telefono:");
+
+        TextView textView = findViewById(R.id.textView3);
+
+        textView.setText("Nombre: "+nomb+"\n\nApellido: "+apell+"\n\nTelefono: "+telef);
+
     }
 }
--- app/src/main/java/hn/uth/pm012024p1/MainActivity.java
+++ app/src/main/java/hn/uth/pm012024p1/MainActivity.java
@@ -2,16 +2,21 @@ package hn.uth.pm012024p1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import Configuracion.SQLiteConexion;
+import Configuracion.Transacciones;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText nombres,apellidos,telefono;
-
     Button btnproceso;
 
     @Override
@@ -27,9 +32,27 @@ public class MainActivity extends AppCompatActivity {
         btnproceso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),nombres.getText().toString(),Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(getApplicationContext(),ActivityPage.class);
+
+                String nomb = nombres.getText().toString();
+                String apell = apellidos.getText().toString();
+                String telef = telefono.getText().toString();
+
+                intent.putExtra("Nombre:",nomb);
+                intent.putExtra("Apellido:",apell);
+                intent.putExtra("Telefono:",telef);
+                startActivity(intent);
+            Addperson();
             }
         });
+    }
+
+    private void Addperson() {
+        SQLiteConexion conexion = new SQLiteConexion(this, Transacciones.DBName,null, 1);
+        SQLiteDatabase db = conexion.getWritableDatabase();
+
+        ContentValues valores = new ContentValues();
+        valores.put(Transacciones.nombres, nombres.getText().toString());
     }
 
 }
